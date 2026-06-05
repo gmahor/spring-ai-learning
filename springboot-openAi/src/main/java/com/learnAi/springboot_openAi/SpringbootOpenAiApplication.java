40727636cb64d3e4092711fa57461665d587dfd2
gmahor <gouravmahor60@gmail.com>
2026-06-05 15:36:36 +0530

    first commit

---
+++ springboot-openAi/src/main/java/com/learnAi/springboot_openAi/SpringbootOpenAiApplication.java
@@ -0,0 +1,13 @@
+package com.learnAi.springboot_openAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOpenAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOpenAiApplication.class, args);
+	}
+
+}
---
+++ springboot-openAi/src/test/java/com/learnAi/springboot_openAi/SpringbootOpenAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnAi.springboot_openAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOpenAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
