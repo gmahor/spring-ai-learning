d3eeb80cab944c2c9f7098f695ed2d8809af7678
gmahor <gouravmahor60@gmail.com>
2026-06-05 17:20:09 +0530

    how to setup gemma3 in docker

---
+++ springboot-gemma3-docker/src/test/java/com/learningai/SpringbootGemma3DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGemma3DockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
